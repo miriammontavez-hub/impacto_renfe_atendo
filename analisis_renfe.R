1210733f23509705c11ba20e2383eb544498ef2d
miriammontavez <miriammontavez@gmail.com>
2025-09-08 17:29:07 +0200

    Actualizacion_2025_09_08

--- analisis_renfe.R
+++ analisis_renfe.R
@@ -1,5 +1,7 @@
 #PROCESAR
 
+setwd("~/Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final")
+
 #Librerias
 
 install.packages("tidyverse")
@@ -11,29 +13,29 @@ library(ggrepel)
 
 #Importar csv
 
-listado_de_estaciones_con_servicio_de_atendo <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/listado-de-estaciones-con-servicio-de-atendo.csv", delim = ";", trim_ws = TRUE, col_types = cols(`Codigo de Estación` = col_integer())) %>%
+listado_de_estaciones_con_servicio_de_atendo <- read_delim("datos/csv/listado-de-estaciones-con-servicio-de-atendo.csv", delim = ";", trim_ws = TRUE, col_types = cols(`Codigo de Estación` = col_integer())) %>%
   clean_names() %>%
   rename(codigo = codigo_de_estacion)
-listado_de_estaciones_ave_ld_md <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/listado_completo_av_ld_md.csv", delim = ";", trim_ws = TRUE, locale = locale(decimal_mark = ","), col_types = cols(`CÓDIGO` = col_integer())) %>%
+listado_de_estaciones_ave_ld_md <- read_delim("datos/csv/listado_completo_av_ld_md.csv", delim = ";", trim_ws = TRUE, locale = locale(decimal_mark = ","), col_types = cols(`CÓDIGO` = col_integer())) %>%
   clean_names()
-estaciones <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/estaciones.csv", delim = ";", trim_ws = TRUE, col_types = cols(`CODIGO` = col_integer())) %>%
+estaciones <- read_delim("datos/csv/estaciones.csv", delim = ";", trim_ws = TRUE, col_types = cols(`CODIGO` = col_integer())) %>%
   clean_names()
-distribucion_por_dicapacidad <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/distribucion-de-las-asistencias-por-tipo-de-discapacidad.csv", delim = ";", col_types = cols(`Persona mayor` = col_number(), `Persona con carrito de niño` = col_number(), `PMR miembros superiores/inferiores` = col_number(), Embarazadas = col_number(), `Discapacidad intelectual` = col_number(), `Discapacidad auditiva` = col_number(), `Dicapacidad visual` = col_number(), `Silla en plaza regular` = col_number(), `Silla en plaza H` = col_number(), `Personas con sordoceguera` = col_number(), `Otros PMR` = col_number()), locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
+distribucion_por_dicapacidad <- read_delim("datos/csv/distribucion-de-las-asistencias-por-tipo-de-discapacidad.csv", delim = ";", col_types = cols(`Persona mayor` = col_number(), `Persona con carrito de niño` = col_number(), `PMR miembros superiores/inferiores` = col_number(), Embarazadas = col_number(), `Discapacidad intelectual` = col_number(), `Discapacidad auditiva` = col_number(), `Dicapacidad visual` = col_number(), `Silla en plaza regular` = col_number(), `Silla en plaza H` = col_number(), `Personas con sordoceguera` = col_number(), `Otros PMR` = col_number()), locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
   clean_names()
-indicadores_de_calidad_osp <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/indicadores-de-calidad-osp.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
+indicadores_de_calidad_osp <- read_delim("datos/csv/indicadores-de-calidad-osp.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
   clean_names()
-indicadores_de_calidad_sc <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/indicadores-de-calidad-servicios-comerciales.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
+indicadores_de_calidad_sc <- read_delim("datos/csv/indicadores-de-calidad-servicios-comerciales.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
   clean_names()
-numero_de_asistencias <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/numero-de-asistencias-del-servicio-atendo.csv", delim = ";", trim_ws = TRUE) %>%
+numero_de_asistencias <- read_delim("datos/csv/numero-de-asistencias-del-servicio-atendo.csv", delim = ";", trim_ws = TRUE) %>%
   clean_names()
-numero_de_empleados <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/numero-de-empleados.csv", delim = ";", trim_ws = TRUE) %>%
+numero_de_empleados <- read_delim("datos/csv/numero-de-empleados.csv", delim = ";", trim_ws = TRUE) %>%
   clean_names() %>%
   rename(ano = anos)
-indicadores_de_calidad_sc <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/indicadores-de-calidad-servicios-comerciales.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
+indicadores_de_calidad_sc <- read_delim("datos/csv/indicadores-de-calidad-servicios-comerciales.csv", delim = ";", locale = locale(decimal_mark = ","), trim_ws = TRUE) %>%
   clean_names()
-indicadores_economicos <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/principales-indicadores-economicos.csv", delim = ";", locale = locale(decimal_mark = ",", grouping_mark = "."), trim_ws = TRUE) %>%
+indicadores_economicos <- read_delim("datos/csv/principales-indicadores-economicos.csv", delim = ";", locale = locale(decimal_mark = ",", grouping_mark = "."), trim_ws = TRUE) %>%
   clean_names()
-indicadores_sociales <- read_delim("Cosas de estudiar/Cursos/Programación/Coursera/Google Data Analyst/Proyecto final/datos/csv/principales-indicadores-sociales.csv", delim = ";", locale = locale(decimal_mark = ",", grouping_mark = "."), trim_ws = TRUE) %>%
+indicadores_sociales <- read_delim("datos/csv/principales-indicadores-sociales.csv", delim = ";", locale = locale(decimal_mark = ",", grouping_mark = "."), trim_ws = TRUE) %>%
   clean_names()
 
 #Crear una sola tabla de datos anuales
@@ -60,6 +62,10 @@ datos_atendo_elegidos <- select(estaciones_atendo, c(codigo, nombre_de_la_estaci
 datos_anuales_elegidos <- select(datos_anuales, c(ano, no_de_asistencias_renfe_atendo, no_de_empleados, viajeros_millones, persona_mayor, persona_con_carrito_de_nino, pmr_miembros_superiores_inferiores, embarazadas, discapacidad_intelectual, discapacidad_auditiva, dicapacidad_visual, silla_en_plaza_regular, silla_en_plaza_h, personas_con_sordoceguera, otros_pmr)) %>%
   rename(n_asistencias = no_de_asistencias_renfe_atendo, n_empleados = no_de_empleados, n_viajeros = viajeros_millones, carrito = persona_con_carrito_de_nino, pmr_miembros = pmr_miembros_superiores_inferiores, disc_intelectual = discapacidad_intelectual, disc_auditiva = discapacidad_auditiva, disc_visual = dicapacidad_visual, sordoceguera = personas_con_sordoceguera) %>%
   mutate(tibble(n_viajeros = n_viajeros * 1e+06, persona_mayor = persona_mayor / 100, carrito = carrito / 100, pmr_miembros = pmr_miembros / 100, embarazadas = embarazadas /100, disc_intelectual = disc_intelectual / 100, disc_auditiva = disc_auditiva / 100, disc_visual = disc_visual / 100, silla_en_plaza_regular = silla_en_plaza_regular / 100, silla_en_plaza_h = silla_en_plaza_h / 100, sordoceguera = sordoceguera / 100, otros_pmr = otros_pmr / 100))
+
+#Guardar estas tablas para el md
+write.csv(datos_atendo_elegidos, file = "datos_atendo_elegidos.csv")
+write.csv(datos_anuales_elegidos, file = "datos_anuales_elegidos.csv")
 
 #Creación de los gráficos
 
